036957a6e181267eacb6cb461cb52e8547df2cca
yksrep <yksrep@gmail.com>
2013-12-06 21:55:42 +1000

    fix

--- src/hydroblocks/blocks/Blocks.java
+++ src/hydroblocks/blocks/Blocks.java
@@ -8,20 +8,21 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class Blocks {
 public static Block block;
+public static Block hydromixerblock;
 
 public static void init() {
 block = new TutBlock(Ids.tutBlock_actual);
 GameRegistry.registerBlock(block, Names.tutBlock_name);
 
-block = new MixerBlock(Ids.mixerBlock_actual);
-GameRegistry.registerBlock(block, Names.mixerBlock_name);
+hydromixerblock = new MixerBlock(Ids.mixerBlock_actual);
+GameRegistry.registerBlock(hydromixerblock, Names.mixerBlock_name);
 
 
 }
 
 public static void addNames() {
 LanguageRegistry.addName(block, Names.tutBlock_name);
-LanguageRegistry.addName(block, Names.mixerBlock_name);
+LanguageRegistry.addName(hydromixerblock, Names.mixerBlock_name);
 
 
 }
